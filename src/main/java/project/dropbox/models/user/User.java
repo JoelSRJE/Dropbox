88b2d8d5707447ef5be8feb025dacc3431c9dcd3
JoelSRJE <JoelSRJE@hotmail.com>
2026-02-02 13:39:20 +0100

    User model, updated its repo

--- src/main/java/project/dropbox/models/user/User.java
+++ src/main/java/project/dropbox/models/user/User.java
@@ -1,5 +1,30 @@
 package project.dropbox.models.user;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(unique = true)
+    private String username;
+
+    private String passwordHash;
+
+    public User(String username, String passwordHash) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
 }
--- src/main/java/project/dropbox/repositories/user/UserRepository.java
+++ src/main/java/project/dropbox/repositories/user/UserRepository.java
@@ -1,6 +1,9 @@
 package project.dropbox.repositories.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.dropbox.models.user.User;
 
-public interface UserRepository extends JpaRepository<> {
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
